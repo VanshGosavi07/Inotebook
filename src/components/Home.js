f45cb8e5d04dc1a5b8941d2bd8e76d8841bc6da8
VanshGosavi07 <vanshgosavi7@gmail.com>
2024-02-26 08:24:56 +0530

    Uselocation hook and form is added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,31 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      This is home
+      <div className="container my-3">
+        <h2>Add a Notes</h2>
+        <form class="my-2">
+          <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Email</label>
+            <input type="email" class="form-control" id="inputEmail4" />
+          </div>
+          <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">Password</label>
+            <input type="password" class="form-control" id="inputPassword4" />
+          </div>
+          <div class="col-12">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" id="gridCheck" />
+              <label class="form-check-label" for="gridCheck">
+                Check me out
+              </label>
+            </div>
+          </div>
+          <div class="col-12">
+            <button type="submit" class="btn btn-primary">Sign in</button>
+          </div>
+        </form>
+        <h2>You Notes</h2>
+      </div>
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,24 +2,9 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    "name": "vansh",
-    "class": "IT"
-  };
-
-  const [state, setState] = useState(s1);
-
-  const update = () => {
-    setTimeout(() => {
-      setState({
-        "name": "ram",
-        "class": "civil"
-      });
-    }, 1000);
-  };
 
   return (
-    <NoteContext.Provider value={{ state, update }}>
+    <NoteContext.Provider value={{}}>
       {props.children}
     </NoteContext.Provider>
   );
