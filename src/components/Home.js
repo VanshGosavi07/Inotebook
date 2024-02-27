abccac8cb02be0b7c6d2094596b2a68b43547636
VanshGosavi07 <vanshgosavi7@gmail.com>
2024-02-27 08:57:35 +0530

    try to fetch notes from context

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,32 +1,37 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import noteContext from '../context/notes/noteContext';
 const Home = () => {
+  const context = useContext(noteContext)
+  const { notes, setNotes } = context;
   return (
     <div>
       <div className="container my-3">
         <h2>Add a Notes</h2>
-        <form class="my-2">
-          <div class="col-md-6">
-            <label for="inputEmail4" class="form-label">Email</label>
-            <input type="email" class="form-control" id="inputEmail4" />
+        <form className="my-2">
+          <div className="col-md-6">
+            <label htmlFor="inputEmail4" className="form-label">Email</label>
+            <input type="email" className="form-control" id="inputEmail4" />
           </div>
-          <div class="col-md-6">
-            <label for="inputPassword4" class="form-label">Password</label>
-            <input type="password" class="form-control" id="inputPassword4" />
+          <div className="col-md-6">
+            <label htmlFor="inputPassword4" className="form-label">Password</label>
+            <input type="password" className="form-control" id="inputPassword4" />
           </div>
-          <div class="col-12">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" id="gridCheck" />
-              <label class="form-check-label" for="gridCheck">
+          <div className="col-12">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" id="gridCheck" />
+              <label className="form-check-label" htmlFor="gridCheck">
                 Check me out
               </label>
             </div>
           </div>
-          <div class="col-12">
-            <button type="submit" class="btn btn-primary">Sign in</button>
+          <div className="col-12">
+            <button type="submit" className="btn btn-primary">Sign in</button>
           </div>
         </form>
         <h2>You Notes</h2>
+        {notes.map((note) => {
+          return note.title + " , "
+        })}
       </div>
     </div>
   )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,12 +2,50 @@ import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-
+  const notesInitial = [
+    {
+      "_id": "65d2f72afe851bb3cec80d59",
+      "user": "65c43dbb4c9f27d58fc4a4b4",
+      "title": "my title 1",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2024-02-19T06:37:30.217Z",
+      "__v": 0
+    },
+    {
+      "_id": "65d2f812fcd66df7cc4f1534",
+      "user": "65c43dbb4c9f27d58fc4a4b4",
+      "title": "my title 2",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2024-02-19T06:41:22.288Z",
+      "__v": 0
+    },
+    {
+      "_id": "65d4bd2f5fbcb78d70125bf3",
+      "user": "65c43dbb4c9f27d58fc4a4b4",
+      "title": "my title 3",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2024-02-20T14:54:39.084Z",
+      "__v": 0
+    },
+    {
+      "_id": "65d4be41ab5c123247c8bb33",
+      "user": "65c43dbb4c9f27d58fc4a4b4",
+      "title": "my title hain na",
+      "description": "Please wake up early",
+      "tag": "personal",
+      "date": "2024-02-20T14:59:13.722Z",
+      "__v": 0
+    }
+  ]
+  const [notes, setNotes] = useState(notesInitial);
   return (
-    <NoteContext.Provider value={{}}>
+    <NoteContext.Provider value={{ notes, setNotes }}>
       {props.children}
     </NoteContext.Provider>
   );
-}
+};
 
 export default NoteState;
